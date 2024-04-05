2da641e7956c09ab6bb7f04d04b5cf0e15cc92b9
reako99 <zldrgx@naver.com>
2024-04-06 00:18:29 +0900

    update gradle

--- src/main/java/bjc/project/soop/controller/HomeController.java
+++ src/main/java/bjc/project/soop/controller/HomeController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class HomeController {
 
     @GetMapping("/")
     public String first(){
-        System.out.println("MainController.main");
-        return "first";
+        System.out.println("HomeController.main");
+        return "home";
     }
 }

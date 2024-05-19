99d55f34e2f49162fb20bc91402289f7282e6111
reako99 <zldrgx@naver.com>
2024-05-19 11:57:18 +0900

    Update BjcProjectApplicationTests.java

--- src/test/java/bjc/project/soop/BjcProjectApplicationTests.java
+++ src/test/java/bjc/project/soop/BjcProjectApplicationTests.java
@@ -18,6 +18,8 @@ class BjcProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("member");
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction tx = em.getTransaction();
